5ed5aa0b51af2d65ac7e0a4a6b82ad08edb82545
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-09-13 17:41:10 +0300

    sliders functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,39 @@
 import MainSlider from "./modules/slider/slider-main";
 import VideoPlayer from "./modules/playVideo";
+import MiniSlider from "./modules/slider/slider-mini";
 
 window.addEventListener("DOMContentLoaded", () => {
-   const slider = new MainSlider({btns: '.next', page: '.page'});
+   const slider = new MainSlider({btns: '.next', container: '.page'});
    slider.render(); 
 
    const player = new VideoPlayer('.showup .play', '.overlay');
    player.init();
+
+   const showUpSlider = new MiniSlider({
+      container: '.showup__content-slider', 
+      prev: '.showup__prev', 
+      next: '.showup__next',
+      activeClass: 'card-active',
+      animate: true
+   });
+
+   showUpSlider.init();
+
+   const modulesSlider = new MiniSlider ({
+      container: '.modules__content-slider', 
+      prev: '.modules__info-btns .slick-prev', 
+      next: '.modules__info-btns .slick-next',
+      activeClass: 'card-active',
+      animate: true
+   });
+   modulesSlider.init();
+
+   const feedSlider = new MiniSlider ({
+      container: '.feed__slider', 
+      prev: '.feed__slider .slick-prev', 
+      next: '.feed__slider .slick-next',
+      activeClass: 'feed__item-active',
+   });
+   feedSlider.init();
+
 });
---
+++ src/js/modules/slider/slider-mini.js
@@ -0,0 +1,74 @@
+import Slider from "./slider";
+
+export default class MiniSlider extends Slider {
+    constructor(container, next, prev, activeClass, animate, autoplay) {
+        super(container, next, prev, activeClass, animate, autoplay);
+    }
+
+    decorizeSlides() {
+        Array.from(this.slides).forEach(slide => {
+            slide.classList.remove(this.activeClass);
+            if (this.animate) {
+                slide.querySelector('.card__title').style.opacity = '0.4';
+                slide.querySelector('.card__controls-arrow').style.opacity = '0';
+            }
+        });
+        
+        if(!this.slides[0].closest('button')){
+                this.slides[0].classList.add(this.activeClass);
+            }
+        
+        if (this.animate) {
+            this.slides[0].querySelector('.card__title').style.opacity = '1';
+            this.slides[0].querySelector('.card__controls-arrow').style.opacity = '1';
+        }
+    }
+
+    bindTriggers() {
+        this.next.addEventListener("click", () => {
+                    //prevent buttons from moving up like slides
+            if (this.slides[1].tagName === 'BUTTON' && this.slides[2].tagName === 'BUTTON') {
+                this.container.appendChild(this.slides[0]);
+                this.container.appendChild(this.slides[1]);
+                this.container.appendChild(this.slides[2]);
+                this.decorizeSlides();
+
+            } else if (this.slides[1].tagName === 'BUTTON') {
+                this.container.appendChild(this.slides[0]);
+                this.container.appendChild(this.slides[1]);
+                this.decorizeSlides();
+
+            } else {
+                this.container.appendChild(this.slides[0]);
+                this.decorizeSlides();
+            }
+
+        });
+
+        this.prev.addEventListener("click", () => { 
+            // this.container.prepend(this.slides[this.slides.length - 1]);
+                //when sees button - teleports it down
+            for (let i = this.slides.length -1 ; i > 0; i--) {
+                if( this.slides[i].tagName !== 'BUTTON') {
+                    this.container.insertBefore(this.slides[i], this.slides[0]);
+                    break;
+                }
+            }
+            this.container.insertBefore(this.slides[this.slides.length - 1], this.slides[0]);
+            this.decorizeSlides();
+        })
+    }
+
+    init() {
+        this.container.style.cssText = `
+        display: flex;
+        flex-wrap: wrap;
+        overflow: hidden;
+        align-item: flex-start;
+        `;
+
+        this.bindTriggers();
+        this.decorizeSlides();
+    }
+}
+
