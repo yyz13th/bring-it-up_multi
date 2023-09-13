da37f14f5ca16a3df0e0a62522e41a2010c5a335
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-09-13 13:07:34 +0300

    sliding screen on slide 3

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -14,6 +14,21 @@ export default class Slider {
             this.slideIndex = this.slides.length;
         };
 
+
+        try {
+            this.hanson.style.opacity = 0;
+
+            if (n === 3) {
+                setInterval(() => {
+                    this.hanson.style.opacity = 1;
+                    this.hanson.classList.add("animated", "slideInUp");
+                }, 3000);
+            } else {
+                this.hanson.style.opacity = 0;
+                this.hanson.classList.remove("animated", "slideInUp");
+            }
+        }catch(e){}
+
         Array.from(this.slides).forEach(slide => {
             slide.style.display = "none";
             // slide.classlist.remove("animated", "slideInDown");
@@ -26,6 +41,11 @@ export default class Slider {
         this.showSlides(this.slideIndex += n);
     }
     render() {
+        try {
+            this.hanson = document.querySelector(".hanson");
+        } catch (e) {}
+       
+
         this.btns.forEach(btn => {
             btn.addEventListener("click", () => {
                 this.plusSlides(1);
@@ -39,5 +59,6 @@ export default class Slider {
         });
 
         this.showSlides(this.slideIndex);
+
     }
 }
