371ed09871eeba265c73f2b86f83713378355ab5
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-10-17 15:35:49 +0300

    deploy fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-import MainSlider from "./modules/slider/slider-main";
-import VideoPlayer from "./modules/playVideo";
-import MiniSlider from "./modules/slider/slider-mini";
-import Difference from "./modules/difference";
-import Form from "./modules/form";
-import ShowInfo from "./modules/showInfo";
-import Download from "./modules/download";
+import MainSlider from "./modules/slider/slider-main.js";
+import VideoPlayer from "./modules/playVideo.js";
+import MiniSlider from "./modules/slider/slider-mini.js";
+import Difference from "./modules/difference.js";
+import Form from "./modules/form.js";
+import ShowInfo from "./modules/showInfo.js";
+import Download from "./modules/download.js";
 
 window.addEventListener("DOMContentLoaded", () => {
    const slider = new MainSlider({btns: '.next', container: '.page'});
